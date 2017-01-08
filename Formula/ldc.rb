4383cce5f4e69f468aece7ed596e22d3a7fc1080
Timothee Cour <timothee.cour@gmail.com>
2017-01-08 05:23:35 -0800

    Allow to build shared libs

--- Formula/ldc.rb
+++ Formula/ldc.rb
@@ -40,6 +40,8 @@ class Ldc < Formula
     end
   end
 
+  option "with-shared-libs", "build shared libs"
+
   needs :cxx11
 
   depends_on "cmake" => :build
@@ -53,9 +55,11 @@ class Ldc < Formula
 
     ENV.cxx11
     (buildpath/"ldc-lts").install resource("ldc-lts")
+    is_shared = (build.with? "shared-libs") ? "ON" : "OFF"
     cd "ldc-lts" do
       mkdir "build" do
         args = std_cmake_args + %W[
+          -DBUILD_SHARED_LIBS=#{is_shared}
           -DLLVM_ROOT_DIR=#{Formula["llvm"].opt_prefix}
         ]
         system "cmake", "..", *args
@@ -64,6 +68,7 @@ class Ldc < Formula
     end
     mkdir "build" do
       args = std_cmake_args + %W[
+        -DBUILD_SHARED_LIBS=#{is_shared}
         -DLLVM_ROOT_DIR=#{Formula["llvm"].opt_prefix}
         -DINCLUDE_INSTALL_DIR=#{include}/dlang/ldc
         -DD_COMPILER=#{buildpath}/ldc-lts/build/bin/ldmd2
