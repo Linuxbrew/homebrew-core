3e4f0299b3fdf597faed3901c00148fcac2c77a2
Shaun Jackman <sjackman@gmail.com>
2017-02-25 12:13:36 -0800

    infer: Depends on zlib for Linuxbrew

--- Formula/infer.rb
+++ Formula/infer.rb
@@ -26,7 +26,10 @@ class Infer < Formula
   depends_on "opam" => :build
   depends_on "pkg-config" => :build
   depends_on "libffi"
-  depends_on "homebrew/dupes/ncurses" unless OS.mac?
+  unless OS.mac?
+    depends_on "zlib"
+    depends_on "homebrew/dupes/ncurses"
+  end
 
   def install
     if build.without?("clang") && build.without?("java")
