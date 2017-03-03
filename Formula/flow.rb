644d174d166c2fcca4f43f52f1ddeb67dbed99e2
Shaun Jackman <sjackman@gmail.com>
2017-03-03 11:17:13 -0800

    flow: Use libelf rather than elfutils for Linuxbrew

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -16,7 +16,7 @@ class Flow < Formula
 
   depends_on "ocaml" => :build
   depends_on "ocamlbuild" => :build
-  depends_on "elfutils" unless OS.mac?
+  depends_on "libelf" unless OS.mac?
 
   env :super if OS.linux?
 
