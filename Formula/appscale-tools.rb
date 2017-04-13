488800c4064202c21aeb6ae6a4da186c607748ca
Maxim Belkin <maxim.belkin@gmail.com>
2017-04-13 15:56:37 -0500

    appscale-tools: depend on libffi for Linuxbrew

--- Formula/appscale-tools.rb
+++ Formula/appscale-tools.rb
@@ -17,6 +17,7 @@ class AppscaleTools < Formula
   depends_on "libyaml"
   depends_on "ssh-copy-id"
   depends_on "openssl"
+  depends_on "libffi" unless OS.mac?
 
   resource "termcolor" do
     url "https://files.pythonhosted.org/packages/8a/48/a76be51647d0eb9f10e2a4511bf3ffb8cc1e6b14e9e4fab46173aa79f981/termcolor-1.1.0.tar.gz"
