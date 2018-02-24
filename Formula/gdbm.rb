8e664c3cb8824efc12de6501f6b4c0d2ad414094
okhowang(王沛文) <okhowang@tencent.com>
2018-02-24 16:01:44 +0800

    fix gdbm on linux by adding _GNU_SOURCE

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -31,6 +31,7 @@ class Gdbm < Formula
     ]
 
     args << "--enable-libgdbm-compat" if build.with? "libgdbm-compat"
+    args << "CFLAGS=-D_GNU_SOURCE" if OS.linux?
 
     system "./configure", *args
     system "make", "install"
