2ca33dbdf35b7554cb46a86d2bd84552070d1fe1
Torsten Seemann <torsten.seemann@gmail.com>
2018-02-27 17:54:09 +1100

    e2fsprogs - enable shared libs too

--- Formula/e2fsprogs.rb
+++ Formula/e2fsprogs.rb
@@ -3,6 +3,7 @@ class E2fsprogs < Formula
   homepage "https://e2fsprogs.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.43.9/e2fsprogs-1.43.9.tar.gz"
   sha256 "5be0ffc01b9720a3f3ccfc86396016baf1334b98751fefa09e0c63eaffdc3897"
+  revision 1 unless OS.mac?
 
   head "https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git"
 
@@ -10,7 +11,6 @@ class E2fsprogs < Formula
     sha256 "d3d586958ff43cf2bbb58bfa47773585c1023931020001b942211fa57b51b3fc" => :high_sierra
     sha256 "57fcc0cb4cff5a0a29a0226615ca397ae56f1df46796d43f2b9155a56dbdfd96" => :sierra
     sha256 "cbfd5ec222f8d56ffdc13f9e42c85893a1e9b7621ec8e4b9f07bb9be7843812a" => :el_capitan
-    sha256 "89dd34baaaf60cf1cc32e21df909a3eeb744e8374d5e1bbaac110591451dfb5c" => :x86_64_linux
   end
 
   keg_only "this installs several executables which shadow macOS system commands"
@@ -19,7 +19,10 @@ class E2fsprogs < Formula
   depends_on "gettext"
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-e2initrd-helper"
+    system "./configure",
+           "--prefix=#{prefix}",
+           "--disable-e2initrd-helper",
+           *("--enable-elf-shlibs" unless OS.mac?)
     system "make"
     system "make", "install"
     system "make", "install-libs"
