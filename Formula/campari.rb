1d606371c2efb1b1cd2007581d7b4a0c23dc41f1
Davide Garolini <dgarolini@gmail.com>
2018-07-18 16:00:51 +0200

    campari 3.0.1 (new formula)
    This software has been already uploaded for launchpad and it is
    fully GPL. It is made for analysing and simulating Molecular
    Dynamics trajectories. We already compiled from source on MacOS
    but we wanted to make the software available for a broader community.
    The tests are still to complete, but we do extensive testing on gitlab
    runners and the software is fully maintained since 2009 (its foundation).

--- Formula/campari.rb
+++ Formula/campari.rb
@@ -3,7 +3,7 @@ class Campari < Formula
   homepage "https://campari.sourceforge.io/"
   url "https://gitlab.com/CaflischLab/debcampari/-/archive/master/debcampari-master.tar.gz"
   version "3.0.1"
-  sha256 "5b9481030d24741d1b4b5e63b48055fb308b534f403975a8f4c0d2f235577e3b"
+  sha256 "8f4a7e256ea53deb8ce3c2607a86deecb9db35b9a2e49e13cae411cabc83df59"
   depends_on "automake"
   depends_on "fftw"
   depends_on "gcc"
@@ -17,12 +17,24 @@ class Campari < Formula
                           "--enable-mpi",
                           "--enable-threads",
                           "--prefix=#{prefix}"
+    bin.mkpath
+    man.mkpath
+    lib.mkpath
+    doc.mkpath
+    share.mkpath
     system "make", "all_brew"
-    Dir.pwd
+    bin.install "campari"
+    lib.install "../lib/*"
+    doc.install "../doc/*"
+    share.install "../params"
+    share.install "../data"
+    share.install "../tools"
+    share.install "../examples"
+    prefix.install_metafiles
     Dir.chdir("..")
   end
   test do
-    Dir.chdir("examples/tutorial1")
+    Dir.chdir("#{share}/examples/tutorial1")
     system "#{bin}/campari", "-k", "TEMPLATE.key"
   end
 end
