f539fbdc92ba359602ab7890a612cb3a6afe4ef2
Maxim Belkin <maxim.belkin@gmail.com>
2018-08-24 14:47:59 +0000

    libtirpc: fix tests and up to 1.0.3

--- Formula/libtirpc.rb
+++ Formula/libtirpc.rb
@@ -1,8 +1,8 @@
 class Libtirpc < Formula
   desc "Port of Sun's Transport-Independent RPC library to Linux"
   homepage "http://sourceforge.net/projects/libtirpc/"
-  url "https://downloads.sourceforge.net/project/libtirpc/libtirpc/1.0.1/libtirpc-1.0.1.tar.bz2"
-  sha256 "5156974f31be7ccbc8ab1de37c4739af6d9d42c87b1d5caf4835dda75fcbb89e"
+  url "https://downloads.sourceforge.net/project/libtirpc/libtirpc/1.0.3/libtirpc-1.0.3.tar.bz2"
+  sha256 "86c3a78fc1bddefa96111dd233124c703b22a78884203c55c3e06b3be6a0fd5e"
   # tag "linuxbrew"
 
   bottle do
@@ -10,7 +10,7 @@ class Libtirpc < Formula
     sha256 "7c093d0948f7794df11cae0a0f0e974e8b0734d46beb2f7977db39d84aa16720" => :x86_64_linux # glibc 2.19
   end
 
-  depends_on "krb5" => :optional unless OS.mac?
+  depends_on "krb5"
 
   def install
     system "./configure",
@@ -19,5 +19,22 @@ class Libtirpc < Formula
       "--disable-silent-rules",
       "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <rpc/des_crypt.h>
+      #include <stdio.h>
+      int main () {
+        char key[] = "My8digitkey1234";
+        if (sizeof(key) != 16)
+          return 1;
+        des_setparity(key);
+        printf("%d\\n", sizeof(key));
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}/tirpc", "-ltirpc", "-o", "test"
+    system "./test"
   end
 end
