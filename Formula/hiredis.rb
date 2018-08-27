58edd3251948f16e0c3ed658f3e8f4fcf777b2c8
Xu Cheng <git@xuc.me>
2018-08-27 18:51:52 +0800

    hiredis: fix test [Linux]

--- Formula/hiredis.rb
+++ Formula/hiredis.rb
@@ -26,8 +26,8 @@ class Hiredis < Formula
   test do
     # running `./test` requires a database to connect to, so just make
     # sure it compiles
-    system ENV.cc, "-I#{include}/hiredis", "-L#{lib}", "-lhiredis",
-           pkgshare/"examples/example.c", "-o", testpath/"test"
+    system ENV.cc, "-I#{include}/hiredis", "-L#{lib}",
+           pkgshare/"examples/example.c", "-o", testpath/"test", "-lhiredis"
     assert_predicate testpath/"test", :exist?
   end
 end
