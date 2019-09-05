15ca426587b539380370d9583c0aeacd25d677d5
Andreas Schöneck <as.maps@gmail.com>
2019-09-05 10:29:11 +0200

    Adopt Nushell bottle for linux
    Fixes #15316

--- Formula/nushell.rb
+++ Formula/nushell.rb
@@ -6,20 +6,13 @@ class Nushell < Formula
   revision 1
   head "https://github.com/nushell/nushell.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "78e24987e5f9697452966b7476733d2125319faf9b9ee746c023769e3cdd92a4" => :mojave
-    sha256 "3e06fbbb3fde18b3ebb2d1e6282a5b0b2eacbc9f29f0703db95c0173514bb13c" => :high_sierra
-    sha256 "8a7835a0ac11682f9f4a95ec92db6617252bb3bb2295996a812f28b47af19002" => :sierra
-  end
-
   depends_on "openssl@1.1"
 
   # Nu requires features from Rust 1.39 to build, so we can't use Homebrew's
   # Rust; picking a known-good Rust nightly release to use instead.
   resource "rust-nightly" do
-    url "https://static.rust-lang.org/dist/2019-08-24/rust-nightly-x86_64-apple-darwin.tar.xz"
-    sha256 "104ddea51b758f4962960097e9e0f3cabf2c671ec3148bc745344431bb93605d"
+    url "https://static.rust-lang.org/dist/2019-09-05/rust-nightly-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "cbc6b9c924a7a16977899116a6789f171a15ea526646c59739df5fee7db00b82"
   end
 
   def install
