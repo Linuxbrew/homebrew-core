79d4874e2ae4df2d1eaceeec0cef8023ffc74c6d
Alois Schlögl <alois.schloegl@gmail.com>
2019-10-20 19:29:19 +0200

    biosig v1.9.5 added (imported from tap schloegl/biosig)

---
+++ Formula/biosig.rb
@@ -0,0 +1,40 @@
+class Biosig < Formula
+  desc "Tools for biomedical signal processing and conversion"
+  homepage "https://biosig.sourceforge.io"
+  url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig4c%2B%2B-1.9.5.src.tar.gz"
+  # version "1.9.5"
+  sha256 "20e72a5a07d1bf8baa649efe437b4d3ed99944f0e4dfc1fbe23bfbe4d9749ed5"
+
+  depends_on "gawk" => :build
+  depends_on "gnu-sed" => :build
+  depends_on "gnu-tar" => :build
+  depends_on "libb64"
+  depends_on "libbiosig"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+
+    system "make"
+    system "make", "install_tools"
+  end
+
+  def uninstall
+    system "make", "uninstall_tools"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test biosig4c%2B%2B`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/save2gdf", "--help"
+  end
+end
