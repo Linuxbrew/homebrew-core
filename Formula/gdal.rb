429e5fc864d9a489cd37dc19d5f09a01498ba7e9
Dawid Dziurla <dawidd0811@gmail.com>
2019-12-30 12:27:04 +0100

    gdal: rebuild for poppler

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -3,13 +3,12 @@ class Gdal < Formula
   homepage "https://www.gdal.org/"
   url "https://download.osgeo.org/gdal/2.4.2/gdal-2.4.2.tar.xz"
   sha256 "dcc132e469c5eb76fa4aaff238d32e45a5d947dc5b6c801a123b70045b618e0c"
-  revision OS.mac? ? 3 : 4
+  revision OS.mac? ? 3 : 5
 
   bottle do
     sha256 "37370723c5db87a87aa05e6601b5ecd91829788770eb3b70b8728483fd42e950" => :catalina
     sha256 "9409de0723bf4c11a9a50c2c5507dba5bf65f31bb0225b1ddea68abc018f992b" => :mojave
     sha256 "d59c9ce6d87b049273cd087bc676ce39b7c650153fe6d6bd5f3c46a261e646ca" => :high_sierra
-    sha256 "05e6026d958d34cd02932081fea1fa2da12d72ac38d8d92a0eafa345559538d5" => :x86_64_linux
   end
 
   head do
