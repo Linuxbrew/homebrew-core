6815325af29ac5d5cad0ecf175d7d84c4d9bd611
Dawid Dziurla <dawidd0811@gmail.com>
2020-02-10 19:18:10 +0100

    chapel: env script with PATH

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -3,20 +3,24 @@ class Chapel < Formula
   homepage "https://chapel-lang.org/"
   url "https://github.com/chapel-lang/chapel/releases/download/1.20.0/chapel-1.20.0.tar.gz"
   sha256 "08bc86df13e4ad56d0447f52628b0f8e36b0476db4e19a90eeb2bd5f260baece"
-  revision 2 unless OS.mac?
+  revision 3 unless OS.mac?
 
   bottle do
     sha256 "057e5c71d41f2ff71434f446ffd8f9aa932b553612313729d4651fdc58233650" => :catalina
     sha256 "8fcaebe6a3c465a29a66e691581b88b2fc9960726e1f94b3f21aa0f53c424044" => :mojave
     sha256 "4aee5a0ddf8a44897a2f03c458a8e7e70d76b07f04024119ed482fbc06cf330c" => :high_sierra
     sha256 "34a5eac538de8fb6ac632109a0154e1d14ff8551bc8f4fec8df8359568697338" => :sierra
-    sha256 "73edd518b795810886de17c81ca93b1b1583c16dafaca6abeb2c0d85b60a0224" => :x86_64_linux
   end
 
   depends_on "python@3.8" unless OS.mac?
 
   def install
-    ENV.prepend_path "PATH", Formula["python@3.8"].opt_libexec/"bin" unless OS.mac?
+    # Fix [Fail] chpl not found. Make sure it available in the current PATH.
+    # Makefile:203: recipe for target 'check' failed
+    unless OS.mac?
+      ENV.prepend_path "PATH", bin
+      ENV.prepend_path "PATH", Formula["python@3.8"].opt_libexec/"bin"
+    end
 
     libexec.install Dir["*"]
     # Chapel uses this ENV to work out where to install.
@@ -43,16 +47,15 @@ class Chapel < Formula
       Hardware::CPU.is_64_bit? ? "linux64-x86_64" : "linux-x86_64"
     end
     bin.install Dir[libexec/"bin/#{platform}/*"]
-    bin.env_script_all_files libexec/"bin/#{platform}/", :CHPL_HOME => libexec
+    if OS.mac?
+      bin.env_script_all_files libexec/"bin/#{platform}/", :CHPL_HOME => libexec
+    else
+      bin.env_script_all_files libexec/"bin/#{platform}/", :CHPL_HOME => libexec, :PATH => ENV["PATH"]
+    end
     man1.install_symlink Dir["#{libexec}/man/man1/*.1"]
   end
 
   test do
-    # Fix [Fail] chpl not found. Make sure it available in the current PATH.
-    # Makefile:203: recipe for target 'check' failed
-    ENV.prepend_path "PATH", bin unless OS.mac?
-    ENV.prepend_path "PATH", Formula["python@3.8"].opt_libexec/"bin" unless OS.mac?
-
     ENV["CHPL_HOME"] = libexec
     cd libexec do
       system "make", "check"
