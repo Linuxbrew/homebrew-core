7df948cd06ff6a4e16cbd0a6d2f49915b6203e08
galaxy001 <galaxy001@gmail.com>
2020-05-15 13:27:26 +0800

    Update r.rb
    Remove `bottle` section.
    use `Formula["tcl-tk"].opt_lib` instead of `#{HOMEBREW_PREFIX}/lib`.

--- Formula/r.rb
+++ Formula/r.rb
@@ -5,13 +5,6 @@ class R < Formula
   sha256 "06beb0291b569978484eb0dcb5d2339665ec745737bdfb4e873e7a5a75492940"
   revision 1
 
-  bottle do
-    sha256 "5905c138df0ad32f3bd822bb0913da1afb28a2b037ff466e8003065d94978414" => :catalina
-    sha256 "fa57206731acfdb0418e84e5bf1c7ef1fbbab8b15597b569039f71ee3337750a" => :mojave
-    sha256 "5e534f3435aa4a48b7874c3a65e1a12ba28fa71d726cc82fdbaa44e3973fb9e6" => :high_sierra
-    sha256 "ebeed59162de9ede2a3f96af02cc63c937d759a036de3f2922c86d90d51e8c04" => :x86_64_linux
-  end
-
   depends_on "pkg-config" => :build
   depends_on "gcc" # for gfortran
   depends_on "gettext"
@@ -66,8 +59,9 @@ class R < Formula
     else
       args << "--with-x"
       args << "--with-tcltk"
-      args << "--with-tcl-config=#{HOMEBREW_PREFIX}/lib/tclConfig.sh"
-      args << "--with-tk-config=#{HOMEBREW_PREFIX}/lib/tkConfig.sh"
+      tcl_lib = Formula["tcl-tk"].opt_lib
+      args << "--with-tcl-config=#{tcl_lib}/tclConfig.sh"
+      args << "--with-tk-config=#{tcl_lib}/tkConfig.sh"
     end
 
     unless OS.mac?
