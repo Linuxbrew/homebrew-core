c7bf4434fa4d5549dec743257a440a95f24f67fd
Dawid Dziurla <dawidd0811@gmail.com>
2020-06-30 23:22:22 +0200

    Update po4a.rb

--- Formula/po4a.rb
+++ Formula/po4a.rb
@@ -77,6 +77,10 @@ class Po4a < Formula
 
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
 
+    unless OS.mac?
+      inreplace "Po4aBuilder.pm", "PERL5LIB=lib", "PERL5LIB=lib:#{ENV["PERL5LIB"]}"
+    end
+
     system "perl", "Build.PL", "--install_base", libexec
     system "./Build"
     system "./Build", "install"
