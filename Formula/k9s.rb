a536e51bedd16570576d945f3c534adfbb4e4a2c
Alexander Zigelski <ali@crunchtime.dev>
2020-07-04 12:18:38 +0200

    k9s 0.21.2 (new formula)

---
+++ Formula/k9s.rb
@@ -0,0 +1,20 @@
+class K9s < Formula
+  desc "Kubernetes CLI To Manage Your Clusters In Style!"
+  homepage "https://k9scli.io/"
+  url "https://github.com/derailed/k9s.git",
+      :tag      => "v0.21.2",
+      :revision => "977791627860a0febb3c217a5322702da109ecbb"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags",
+             "-s -w -X github.com/derailed/k9s/cmd.version=#{version}
+             -X github.com/derailed/k9s/cmd.commit=#{stable.specs[:revision]}",
+             *std_go_args
+  end
+
+  test do
+    system "k9s", "version"
+  end
+end
