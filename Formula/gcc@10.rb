1c53f63b5fb5eecca242ec83bbf557af55a20a39
Jonathan Chang <jchang641@gmail.com>
2020-08-02 20:49:35 +1000

    Update gcc@10.rb

--- Formula/gcc@10.rb
+++ Formula/gcc@10.rb
@@ -7,7 +7,6 @@ class GccAT10 < Formula
   mirror "https://ftpmirror.gnu.org/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz"
   sha256 "b8dd4368bb9c7f0b98188317ee0254dd8cc99d1e3a18d0ff146c855fe16c1d8c"
   license "GPL-3.0"
-  head "https://gcc.gnu.org/git/gcc.git"
 
   # gcc is designed to be portable.
   # always add cellar :any
