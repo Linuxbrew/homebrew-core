c9736f401cac4e39a792de428b12e976589bfb5f
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-19 08:03:59 +0200

    Update caddy.rb

--- Formula/caddy.rb
+++ Formula/caddy.rb
@@ -76,7 +76,7 @@ class Caddy < Formula
     fork do
       exec bin/"caddy", "run", "--config", testpath/"Caddyfile"
     end
-    sleep 5
+    system "sleep", "2"
 
     assert_match "\":#{port2}\"",
       shell_output("curl -s http://127.0.0.1:#{port1}/config/apps/http/servers/srv0/listen/0")
