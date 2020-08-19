b92209425b56032fafd7d25e29b227373c1241d1
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-19 08:18:04 +0200

    Update caddy.rb

--- Formula/caddy.rb
+++ Formula/caddy.rb
@@ -73,10 +73,11 @@ class Caddy < Formula
       }
     EOS
 
-    fork do
+    pid = fork do
       exec bin/"caddy", "run", "--config", testpath/"Caddyfile"
     end
-    system "sleep", "2"
+    Process.detach pid
+    sleep 2
 
     assert_match "\":#{port2}\"",
       shell_output("curl -s http://127.0.0.1:#{port1}/config/apps/http/servers/srv0/listen/0")
