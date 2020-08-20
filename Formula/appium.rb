2006e76bb7bc1be061aedeeb4a7891a8afc13dd5
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-20 17:24:45 +0200

    appium: fix

--- Formula/appium.rb
+++ Formula/appium.rb
@@ -17,6 +17,10 @@ class Appium < Formula
 
   depends_on "node"
 
+  on_linux do
+    depends_on "glib"
+  end
+
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
