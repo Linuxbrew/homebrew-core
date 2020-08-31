6e813aae4726034a506baa5b8103c0e5c8ae5a03
Jonathan Chang <jchang641@gmail.com>
2020-08-31 14:04:58 +1000

    fails with gcc 5

--- Formula/bazel.rb
+++ Formula/bazel.rb
@@ -14,8 +14,13 @@ class Bazel < Formula
 
   depends_on "python@3.8" => :build
   depends_on "openjdk@11"
-
   uses_from_macos "zip"
+
+  on_linux do
+    depends_on "gcc@6"
+  end
+
+  fails_with gcc: "5"
 
   def install
     ENV["EMBED_LABEL"] = "#{version}-homebrew"
