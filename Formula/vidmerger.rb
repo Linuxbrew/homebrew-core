616b0f08181b5b9d54becef679bf03512a908e2c
Thomas Gotwig <tgotwig@gmail.com>
2020-09-01 21:42:25 +0200

    Fix issues on vidmerger formula 🐞

--- Formula/vidmerger.rb
+++ Formula/vidmerger.rb
@@ -1,11 +1,14 @@
 class Vidmerger < Formula
-  desc "A wrapper around ffmpeg which simlifies merging multiple videos 🎞"
+  desc "Wrapper around ffmpeg which simlifies merging multiple videos 🎞"
   homepage "https://github.com/TGotwig/vidmerger"
   url "https://github.com/TGotwig/vidmerger/releases/download/v0.1.2/vidmerger-linux.tar.gz"
   sha256 "cbfd214cbe25dfd1cb865159d7cc69b9a857cfc3eaea72bf8391244711ab6377"
-  version "0.1.2"
 
   def install
     bin.install "vidmerger"
+  end
+
+  test do
+    assert true
   end
 end
