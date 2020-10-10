3523b07e4fac0c185a4f9be9a10af0605cc29ba9
Baran Yildirim <32420873+baranyildirim@users.noreply.github.com>
2020-10-10 18:40:38 -0400

    Update bazel.rb

--- Formula/bazel.rb
+++ Formula/bazel.rb
@@ -15,13 +15,13 @@ class Bazel < Formula
   depends_on "python@3.8" => :build
   depends_on "openjdk@11"
   uses_from_macos "zip"
+  on_linux do
+    depends_on "gcc@9"
+  end
   fails_with gcc: "5"
   fails_with gcc: "6"
   fails_with gcc: "7"
   fails_with gcc: "8"
-  on_linux do
-    depends_on "gcc@9"
-  end
 
   def install
     ENV["EMBED_LABEL"] = "#{version}-homebrew"
