093d978217e87016d221afa71f21c8744bb7f3fb
Baran Yildirim <32420873+baranyildirim@users.noreply.github.com>
2020-10-10 22:33:47 -0400

    Update bazel.rb

--- Formula/bazel.rb
+++ Formula/bazel.rb
@@ -14,37 +14,44 @@ class Bazel < Formula
 
   depends_on "python@3.8" => :build
   depends_on "openjdk@11"
-  uses_from_macos "zip"
+  
   on_linux do
+    fails_with gcc: "5"
+    fails_with gcc: "6"
+    fails_with gcc: "7"
+    fails_with gcc: "8"
     depends_on "gcc@9"
   end
-  fails_with gcc: "5"
-  fails_with gcc: "6"
-  fails_with gcc: "7"
-  fails_with gcc: "8"
+
+  uses_from_macos "zip"
 
   def install
     ENV["EMBED_LABEL"] = "#{version}-homebrew"
     # Force Bazel ./compile.sh to put its temporary files in the buildpath
     ENV["BAZEL_WRKDIR"] = buildpath/"work"
     # Force Bazel to use openjdk@11
+    
     ENV["JAVA_HOME"] = if OS.mac?
       Formula["openjdk@11"].opt_libexec/"openjdk.jdk/Contents/Home"
     else
       Formula["openjdk@11"].opt_prefix
     end
+
     ENV["EXTRA_BAZEL_ARGS"] = "--host_javabase=@local_jdk//:jdk --verbose_failures"
     ENV["VERBOSE"] = "yes"
+
     (buildpath/"sources").install buildpath.children
+
     cd "sources" do
       system "./compile.sh",
              "--env=std"
       system "./output/bazel",
              "--output_user_root",
-             "--cxxopt=-std=c++11",
+             *("--cxxopt=-std=c++11" unless OS.mac?),
              buildpath/"output_user_root",
-             "build",
+             "build --cxxopt=-std=c++11",
              "scripts:bash_completion"
+
       bin.install "scripts/packages/bazel.sh" => "bazel"
       ln_s libexec/"bin/bazel-real", bin/"bazel-#{version}"
       (libexec/"bin").install "output/bazel" => "bazel-real"
@@ -77,7 +84,7 @@ class Bazel < Formula
     EOS
 
     system bin/"bazel",
-           "build",
+           "build --cxxopt=-std=c++11",
            "//:bazel-test"
     assert_equal "Hi!\n", pipe_output("bazel-bin/bazel-test")
 
