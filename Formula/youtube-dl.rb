a2b837db2ac173ab12ea49f41e2bf40b49bb5724
Leopoldo Pla <leopoldoplasempere@gmail.com>
2020-10-24 18:52:01 +0200

    Changed source file to pypi and deleted HEAD code

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -1,8 +1,8 @@
 class YoutubeDl < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://yt-dl.org"
-  url "http://abf-downloads.openmandriva.org/ytdl/youtube-dl-2020.09.20.tar.gz"
-  sha256 "ead79e9248aaf7667f015da2ccd4bde44a8948fa980f82965609cdca88ad285e"
+  url "https://files.pythonhosted.org/packages/12/8b/51cae2929739d637fdfbc706b2d5f8925b5710d8f408b5319a07ea45fe99/youtube_dl-2020.9.20.tar.gz"
+  sha256 "67fb9bfa30f5b8f06227c478a8a6ed04af1f97ad4e81dd7e2ce518df3e275391"
   license "Unlicense"
 
   #HEAD is disabled as https://github.com/ytdl-org/youtube-dl/ is taken down due to DMCA takedown notice by RIAA (https://github.com/github/dmca/blob/master/2020/10/2020-10-23-RIAA.md).
