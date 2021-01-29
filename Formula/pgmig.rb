74f3c15dec9e7be71e74ce475fd7ff0b8f8e0ad6
turbo.cafe@clovermail.net <turbo.cafe@clovermail.net>
2021-01-30 01:17:38 +0200

    also add ^Ccos binary

--- Formula/pgmig.rb
+++ Formula/pgmig.rb
@@ -1,10 +1,17 @@
 class Pgmig < Formula
   desc "Standalone PostgreSQL Migration Runner"
   homepage "https://github.com/leafclick/pgmig/"
-  url "https://github.com/leafclick/pgmig/releases/download/v0.5.0/pgmig-0.5.0-linux-amd64.zip"
-  sha256 "eb677803e72c3c1d1f46eddf81ba52e59ab7c5fe4ec702b1809a11237b614db9"
   license "Apache-2.0"
   bottle :unneeded
+
+  if OS.linux?
+    url "https://github.com/leafclick/pgmig/releases/download/v0.5.0/pgmig-0.5.0-linux-amd64.zip"
+    sha256 "eb677803e72c3c1d1f46eddf81ba52e59ab7c5fe4ec702b1809a11237b614db9"
+  else
+    url "https://github.com/leafclick/pgmig/releases/download/v0.5.0/pgmig-0.5.0-macos-amd64.zip"
+    sha256 "87d648ce4515067ed8974cc9c7aacf658f7418ad15828dd2b408c325e377c2a8"
+  end
+
   def install
     bin.install "pgmig"
   end
