af7235acc2b20ec61fd1ec8f6b87a8daa04d092e
Nuri Jung <jnooree@snu.ac.kr>
2021-02-07 09:15:35 +0900

    Mark libxslt as linux-only dependency

--- Formula/libsecret.rb
+++ Formula/libsecret.rb
@@ -21,11 +21,12 @@ class Libsecret < Formula
   depends_on "docbook-xsl" => :build
   depends_on "gettext" => :build
   depends_on "gobject-introspection" => :build
-  depends_on "libxslt" => :build
   depends_on "pkg-config" => :build
   depends_on "vala" => :build
   depends_on "glib"
   depends_on "libgcrypt"
+  
+  depends_on "libxslt" => :build unless OS.mac?
 
   def install
     # Needed by intltool (xml::parser)
