f763bd97938ff99b7872774370f342c0f3ef16c8
Liyang HU <git@liyang.hu>
2021-03-07 04:08:36 +0000

    gcc@9: remove SED=/usr/bin/sed workaround
    Library/Homebrew/shims/mac/super/sed was removed in Homebrew/brew#10802

--- Formula/gcc@9.rb
+++ Formula/gcc@9.rb
@@ -106,9 +106,6 @@ class GccAT9 < Formula
         args << "--with-native-system-header-dir=/usr/include"
         args << "--with-sysroot=#{sdk}"
       end
-
-      # Avoid reference to sed shim
-      args << "SED=/usr/bin/sed"
     end
 
     # Ensure correct install names when linking against libgcc_s;
