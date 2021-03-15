551414f72894c30dc57bd80f2695ff2e1cca47a4
Vince Power <vince@power.sh>
2021-03-15 13:36:13 -0300

    Updating based on brew style guidelines

--- Formula/openshift-cli.rb
+++ Formula/openshift-cli.rb
@@ -1,9 +1,8 @@
 class OpenshiftCli < Formula
-  desc "Red Hat OpenShift command line interface tool"
+  desc "Red Hat OpenShift command-line interface tool"
   homepage "https://www.openshift.com/"
   url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.7.2/openshift-client-linux-4.7.2.tar.gz"
   sha256 "e52795752079527824f7deae88996045ac67e7078eb4331a0793deae7d6500b7"
-  version "4.7.2"
   license "Apache-2.0"
 
   depends_on "kubernetes-cli"
@@ -17,7 +16,5 @@ class OpenshiftCli < Formula
   test do
     run_output = shell_output("#{bin}/oc 2>&1")
     assert_match "OpenShift Client", run_output
-
-    version_output = shell_output("#{bin}/oc version --client 2>&1")
   end
 end
