f30970d5a676f00384292dabc93aa90c366cac96
brewfixvim <81582700+brewfixvim@users.noreply.github.com>
2021-03-31 00:38:53 +0300

    Add vim formula with X11 GUI support
    Add a vim formula for users that want to build with the niceties of X/gtk e.g. clipboard support

---
+++ Formula/vim-gtk.rb
@@ -0,0 +1,80 @@
+class Vim < Formula
+  desc "Vi 'workalike' with many additional features"
+  homepage "https://www.vim.org/"
+  # vim should only be updated every 25 releases on multiples of 25
+  url "https://github.com/vim/vim/archive/v8.2.2650.tar.gz"
+  sha256 "0284b0e66287641b196b29496ea7d44ad5a728da0fc3b8eea36f1ed3636b661e"
+  license "Vim"
+  head "https://github.com/vim/vim.git"
+
+  bottle do
+    sha256 arm64_big_sur: "dcffc10013bbe1dea44820444a04cbad8d4be4475f2997114dd0f3c12880d03c"
+    sha256 big_sur:       "3a263f47258ea4f73b1f340172fa5b43eb2db2df26dcd9b9c716e10e6316e080"
+    sha256 catalina:      "e7b4d38dc82325540a4ab51bab2afec9b1ba3a45bb6ef2aa8f417d5fec14cef3"
+    sha256 mojave:        "f54c421a35334dd678e7cdea6c37eb964e414e962abb5b0e332f637d14edaae0"
+    sha256 x86_64_linux:  "d87a19728ecec53875c19a81bf467881b86dc44544f79a92031fdc3817c87ebf"
+  end
+
+  depends_on "gettext"
+  depends_on "lua"
+  depends_on "perl"
+  depends_on "python@3.9"
+  depends_on "ruby"
+  depends_on "gtk+3"
+
+  uses_from_macos "ncurses"
+
+  conflicts_with "ex-vi",
+    because: "vim and ex-vi both install bin/ex and bin/view"
+
+  conflicts_with "macvim",
+    because: "vim and macvim both install vi* binaries"
+
+  def install
+    ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
+
+    # https://github.com/Homebrew/homebrew-core/pull/1046
+    ENV.delete("SDKROOT")
+
+    # vim doesn't require any Python package, unset PYTHONPATH.
+    ENV.delete("PYTHONPATH")
+
+    # We specify HOMEBREW_PREFIX as the prefix to make vim look in the
+    # the right place (HOMEBREW_PREFIX/share/vim/{vimrc,vimfiles}) for
+    # system vimscript files. We specify the normal installation prefix
+    # when calling "make install".
+    # Homebrew will use the first suitable Perl & Ruby in your PATH if you
+    # build from source. Please don't attempt to hardcode either.
+    system "./configure", "--prefix=#{HOMEBREW_PREFIX}",
+                          "--mandir=#{man}",
+                          "--enable-multibyte",
+                          "--with-tlib=ncurses",
+                          "--with-compiledby=Homebrew",
+                          "--enable-cscope",
+                          "--enable-perlinterp",
+                          "--enable-rubyinterp",
+                          "--enable-python3interp",
+                          "--enable-gui=gtk3",
+                          "--enable-luainterp",
+                          "--with-lua-prefix=#{Formula["lua"].opt_prefix}"
+    system "make"
+    # Parallel install could miss some symlinks
+    # https://github.com/vim/vim/issues/1031
+    ENV.deparallelize
+    # If stripping the binaries is enabled, vim will segfault with
+    # statically-linked interpreters like ruby
+    # https://github.com/vim/vim/issues/114
+    system "make", "install", "prefix=#{prefix}", "STRIP=#{which "true"}"
+    bin.install_symlink "vim" => "vi"
+  end
+
+  test do
+    (testpath/"commands.vim").write <<~EOS
+      :python3 import vim; vim.current.buffer[0] = 'hello python3'
+      :wq
+    EOS
+    system bin/"vim", "-T", "dumb", "-s", "commands.vim", "test.txt"
+    assert_equal "hello python3", File.read("test.txt").chomp
+    assert_match "+gettext", shell_output("#{bin}/vim --version")
+  end
+end
