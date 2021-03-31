089d5090920541db814570237150e9a720f19c74
brewfixvim <81582700+brewfixvim@users.noreply.github.com>
2021-03-31 13:08:35 +0300

    Remove `bottle do` block

--- Formula/vim-gtk.rb
+++ Formula/vim-gtk.rb
@@ -7,14 +7,6 @@ class VimGtk < Formula
   license "Vim"
   head "https://github.com/vim/vim.git"
 
-  bottle do
-    sha256 arm64_big_sur: "dcffc10013bbe1dea44820444a04cbad8d4be4475f2997114dd0f3c12880d03c"
-    sha256 big_sur:       "3a263f47258ea4f73b1f340172fa5b43eb2db2df26dcd9b9c716e10e6316e080"
-    sha256 catalina:      "e7b4d38dc82325540a4ab51bab2afec9b1ba3a45bb6ef2aa8f417d5fec14cef3"
-    sha256 mojave:        "f54c421a35334dd678e7cdea6c37eb964e414e962abb5b0e332f637d14edaae0"
-    sha256 x86_64_linux:  "d87a19728ecec53875c19a81bf467881b86dc44544f79a92031fdc3817c87ebf"
-  end
-
   depends_on "gettext"
   depends_on "gtk+3"
   depends_on "lua"
