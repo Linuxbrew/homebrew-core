67d0681779d8053802760a6383d714a707cf2ade
Michka Popoff <michkapopoff@gmail.com>
2021-04-12 20:01:48 +0200

    dbus-glib: rev-bump for rebuild

--- Formula/dbus-glib.rb
+++ Formula/dbus-glib.rb
@@ -3,6 +3,7 @@ class DbusGlib < Formula
   homepage "https://wiki.freedesktop.org/www/Software/DBusBindings/"
   url "https://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-0.112.tar.gz"
   sha256 "7d550dccdfcd286e33895501829ed971eeb65c614e73aadb4a08aeef719b143a"
+  revision 1 unless OS.mac?
 
   livecheck do
     url "https://dbus.freedesktop.org/releases/dbus-glib/"
