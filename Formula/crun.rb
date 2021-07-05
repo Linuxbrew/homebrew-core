3bf02b0c156eca058204cc2c2555edcb1e23e461
Henry Chu <henrytech@outlook.com>
2021-07-05 12:25:45 +0800

    crun: Fixed version issue

--- Formula/crun.rb
+++ Formula/crun.rb
@@ -1,8 +1,7 @@
 class Crun < Formula
   desc "Fast and lightweight fully featured OCI runtime and C library"
   homepage "https://github.com/containers/crun"
-  version "0.20.1"
-  url "https://github.com/containers/crun/releases/download/#{version}/crun-#{version}.tar.xz"
+  url "https://github.com/containers/crun/releases/download/0.20.1/crun-0.20.1.tar.xz"
   sha256 "d118b9749e0ea90de1bd8bb1314f319f43559bbd275cd9331f2d96cfd1ccf9e0"
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
@@ -24,6 +23,6 @@ class Crun < Formula
   end
 
   test do
-      assert_equal "crun version #{version}", shell_output("crun --version").lines.first.strip
+    assert_equal "crun version #{version}", shell_output("crun --version").lines.first.strip
   end
 end
