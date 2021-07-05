9e70cca226445a355ad51906d409c270e752d7c0
Henry Chu <henrytech@outlook.com>
2021-07-05 12:19:21 +0800

    crun: New formula

---
+++ Formula/crun.rb
@@ -0,0 +1,29 @@
+class Crun < Formula
+  desc "Fast and lightweight fully featured OCI runtime and C library"
+  homepage "https://github.com/containers/crun"
+  version "0.20.1"
+  url "https://github.com/containers/crun/releases/download/#{version}/crun-#{version}.tar.xz"
+  sha256 "d118b9749e0ea90de1bd8bb1314f319f43559bbd275cd9331f2d96cfd1ccf9e0"
+  license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
+
+  depends_on "go-md2man" => :build
+  depends_on "libtool" => :build
+  depends_on "python@3.9" => :build
+
+  depends_on "libcap"
+  depends_on "libseccomp"
+  depends_on :linux
+  depends_on "systemd"
+  depends_on "yajl"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+      assert_equal "crun version #{version}", shell_output("crun --version").lines.first.strip
+  end
+end
