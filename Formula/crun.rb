da29badc9f8b8f66d5da945c76dc13b61abd70ae
Henry Chu <henrytech@outlook.com>
2021-07-05 13:49:46 +0800

    crun: Added missing build dependencies

--- Formula/crun.rb
+++ Formula/crun.rb
@@ -5,8 +5,11 @@ class Crun < Formula
   sha256 "d118b9749e0ea90de1bd8bb1314f319f43559bbd275cd9331f2d96cfd1ccf9e0"
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "go-md2man" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
   depends_on "python@3.9" => :build
 
   depends_on "libcap"
